2184b63a2175632dfd482f5ed7e184ea7d3b7a34
lis55 <lis_lou77@msn.com>
2021-11-14 19:13:44 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"619142907514e1001f25f6f6",
+				"619142907514e1001f25f6f8"
+			]
+		},
+		"619142907514e1001f25f6f6": {
+			"id": "619142907514e1001f25f6f6",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"619142907514e1001f25f6f8": {
+			"id": "619142907514e1001f25f6f8",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,262 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Section, Icon, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml, Override, StackItem, Stack, SocialMedia } from "@quarkly/components";
+import { MdHelp } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0" color="--light">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						margin-right="-20%"
+						padding-bottom="120%"
+						background="url(https://previews.dropbox.com/p/thumb/ABVG--3FIZi5s0V1SQjt6q5gMly8uAx3sqq_iYKychLlUpA_TatfUWURegi1GFcJKMqBpeBPU7dxs3COb9_PEMceJ4TyjQoIK2WQD53_O-b8kp5jA7yjqXXLHLMki948hdHV-dIP8EkDH87mlPHB7tdbJ0n572tI2qAaeeSziHT6Y2zEZtqIcZQ_tuMmQs_1kMn38l_TlkkRhLi-mUPc8Ev70Ib_sGKu1sAWYCBWjMVGiMo3L3G8_acQOu_c4_bWmMWocKlZP9m29NJCw7s688RUn6EkaNqHb-kFn4DKNK4Hq7xozfXF0LGu94EQBGQU0yCQRjX4SGFV_Z1BjR5GOSB8gD4CnKqou6IGS9SoAFsvcQ/p.jpeg) 20% 15%/cover"
+						lg-margin-right="0"
+						lg-padding-bottom="80%"
+					/>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						padding="98px 64px"
+						margin-left="-20%"
+						margin-top="36px"
+						background="--color-primary"
+						color="--light"
+						mix-blend-mode="lighten"
+						lg-margin-left="0"
+						lg-margin-top="0"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="0"
+						font="--base"
+						letter-spacing="1px"
+						text-transform="uppercase"
+						color="#b7d6f5"
+					>
+						About me
+					</Text>
+					<Text
+						as="h1"
+						margin="50px 0px 0px 200px"
+						font="--headline1"
+						lg-font="--headline2"
+						color="--light"
+					>
+						Hey I’m Lis
+					</Text>
+					<Text
+						as="p"
+						margin="20px 200px"
+						font="--base"
+						max-width="400px"
+						color="--lightD2"
+					>
+						Welcome to my page, my name Lis Louise.  I have done my masters in physics at the University of Erlangen-Nuremberg. My focus areas are image processing and machine learning. I have been developing in python for 4 years. Recently I started my own web development project. Feel free to cheack more about me below.{"  "}
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section color="--dark">
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				Experience
+			</Text>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					Is there life on Mars?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					How much salt is in the ocean?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					Why is the sky blue?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+		</Section>
+		<Section color="--dark">
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				Education
+			</Text>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					Is there life on Mars?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					How much salt is in the ocean?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					Why is the sky blue?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+		</Section>
+		<Section padding="60px 0" sm-padding="40px 0">
+			<SocialMedia twitter="https://twitter.com/quarklyapp" youtube="https://www.youtube.com/channel/UCTB-3keeJ8QQzgPGnlcZwCw" telegram="https://t.me/joinchat/DqSYDhS0R9nMRvOtFbIxrQ" linkedIn="https://www.linkedin.com/in/lis-louise-8a8351108/">
+				<Override
+					slot="link"
+					border-radius="50%"
+					color="--light"
+					margin="0 8px"
+					background="--color-grey"
+					hover-background="--color-greyD1"
+				/>
+			</SocialMedia>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"619142907514e1001f25f6f1"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
